562f59c252d95b15a7a01d7e169135c88733e51a
Raymond Feng <raymond@strongloop.com>
2013-06-18 14:43:51 -0700

    Add assert

--- test/rest-adapter-custom.test.js
+++ test/rest-adapter-custom.test.js
@@ -30,10 +30,12 @@ describe('REST connector', function () {
             var ds = new DataSource(require('../lib/rest-adapter'), spec);
             assert(ds.geocode);
             ds.geocode(40.714224,-73.961452, function(err, response) {
-                console.log(response.headers);
+                // console.log(response.headers);
                 var body = response.body;
-                console.log(body.results[0].formatted_address);
-                done(err, body);
+                var address = body.results[0].formatted_address;
+                console.log(address);
+                assert.equal('285 Bedford Avenue, Brooklyn, NY 11211, USA', address);
+                done(err, address);
             });
 
         });
