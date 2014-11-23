c2683f42b2a9b05b1461169f10de43fe18a67c79
Raymond Feng <raymond@strongloop.com>
2014-11-23 10:23:03 -0800

    Allow options to be configured for request module
    1. Allow default options to be configured at connector settings
    2. Allow request options to be configured at operation level

--- example/rest-loopback-geocode.js
+++ example/rest-loopback-geocode.js
@@ -2,19 +2,26 @@ var loopback = require("loopback");
 
 var ds = loopback.createDataSource({
   connector: require("../index"),
+  strictSSL: false,
   debug: false,
+  defaults: {
+    "headers": {
+      "accept": "application/json",
+      "content-type": "application/json"
+    }
+  },
   operations: [
     {
       template: {
         "method": "GET",
         "url": "http://maps.googleapis.com/maps/api/geocode/{format=json}",
-        "headers": {
-          "accept": "application/json",
-          "content-type": "application/json"
-        },
         "query": {
           "address": "{street},{city},{zipcode}",
           "sensor": "{sensor=false}"
+        },
+        "options": {
+          "strictSSL": true,
+          "useQuerystring": true
         },
         "responsePath": "$.results[0].geometry.location"
       },
