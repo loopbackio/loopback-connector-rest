2de4e5e5e8e0294eb126b75c5f16cbbcee85dd43
Ryan Graham <r.m.graham@gmail.com>
2016-04-04 18:41:32 -0700

    insert IBM copyrights

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2013,2016. All Rights Reserved.
+// Node module: loopback-connector-rest
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 module.exports = require('./lib/rest-builder');
 
 var connector = require('./lib/rest-connector');
--- test/express-helper.js
+++ test/express-helper.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
+// Node module: loopback-connector-rest
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 var express = require('express');
 
 module.exports = function createApp() {
